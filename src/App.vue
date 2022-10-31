adbf96a3ff5b1c515c5934d804f9e6b959fdcc2e
Edgar Sanroesdi <e.sanroesdi@oegematransport.nl>
2022-10-31 17:51:54 +0100

    Replace API call with call from API client

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="bg-blue-300 text-white pt-4 overflow-auto">
     <h1 class="text-center text-6xl md:text-8xl" v-if="weatherData">
-      {{ weatherData.data.name }}
+      {{ weatherData.name }}
     </h1>
     <div class="flex flex-col items-center">
       <div class="mt-8 pt-4 p-12 rounded-full">
         <CurrentWeather
           v-if="weatherData"
-          :weatherData="weatherData.data"
+          :weatherData="weatherData"
         ></CurrentWeather>
       </div>
     </div>
@@ -21,11 +21,12 @@
 
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import axios from "axios";
 import CurrentWeather from "./components/CurrentWeather/CurrentWeather.vue";
 import ForecastWeather from "./components/ForecastWeather/ForecastWeather.vue";
 import type { ICurrentWeather } from "./components/CurrentWeather/ICurrentWeather";
-const weatherData = ref<{ data: ICurrentWeather }>();
+import { getWeather } from "./util/apiClient";
+import type { AxiosError } from "axios";
+const weatherData = ref<ICurrentWeather>();
 const currentLocation = ref();
 onMounted(async () => {
   navigator.geolocation.getCurrentPosition(
@@ -48,13 +49,14 @@ const setDefaultLocation = () => {
   fetchData(defaultLocation);
 };
 const fetchData = async (location: { lat: number; lon: number }) => {
-  try {
-    weatherData.value = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&lang=nl&units=metric&appid=6a4ee0163b33f226ca20bb2b1a52c9ed`
-    );
-    console.log(weatherData.value);
-  } catch (error) {
-    console.log(error);
+  const [error, CurrentWeather] = (await getWeather(location)) as [
+    AxiosError,
+    ICurrentWeather
+  ];
+  if (error) {
+    console.error(error);
+    return;
   }
+  weatherData.value = CurrentWeather;
 };
 </script>
